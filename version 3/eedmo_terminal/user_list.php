8a3f790e5b1f3554e376069322b32c13af8b1b70
wendale1231 <spooderm5@gmail.com>
2019-08-19 13:33:44 +0800

    changes for login to optimize the database"please refer"

---
+++ version 3/eedmo_terminal/user_list.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>EEDMO - Dashboard</title>
+
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+  <link href="css/sb-admin.css" rel="stylesheet">
+
+</head>
+
+    <!---SEPARATE HEADER KAY SAMOK SHO -->
+<?php include('includes/header.php');?>
+
+
+  <div id="wrapper">
+
+    <!-- SEPARATE Sidebar -->
+    <?php include('includes/sidebar.php');?>
+
+
+    <div id="content-wrapper">
+
+      <div class="container-fluid">
+
+        <!-- Breadcrumbs-->
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item">
+            <a href="#">Dashboard</a>
+          </li>
+          <li class="breadcrumb-item active">Overview</li>
+        </ol>
+
+        <!-- Icon Cards-->
+        
+              </a>
+            </div>
+          </div>
+        </div>
+
+
+<?php include('includes/footer.php');?>
+    </div>
+    <!-- /.content-wrapper -->
+
+  </div>
+  <!-- /#wrapper -->
+
+
+
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="vendor/chart.js/Chart.min.js"></script>
+  <script src="vendor/datatables/jquery.dataTables.js"></script>
+  <script src="vendor/datatables/dataTables.bootstrap4.js"></script>
+  <script src="js/sb-admin.min.js"></script>
+
+</body>
+
+</html>
