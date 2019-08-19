76b83ca7811719192ac549a47edbc245ff899acd
knightbreaker1 <48872382+knightbreaker1@users.noreply.github.com>
2019-08-19 22:56:31 +0800

    Add files via upload

---
+++ version 3/staff_ibjt/ibjt_index.php
@@ -0,0 +1,205 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/connect.php');
+if(strlen($_SESSION['alogin'])==0)
+  { 
+header('location:index.php');
+}
+else{
+  ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>EEDMO - Dashboard</title>
+
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+  <link href="css/sb-admin.css" rel="stylesheet">
+
+</head>
+
+    <!---SEPARATE HEADER KAY SAMOK SHO -->
+<?php include('includes/header.php');?>
+
+
+  <div id="wrapper">
+
+    <!-- SEPARATE Sidebar -->
+    <?php include('includes/sidebar.php');?>
+
+
+    <div id="content-wrapper">
+
+      <div class="container-fluid">
+
+        <!-- Breadcrumbs-->
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item">
+            <a href="#">Dashboard</a>
+          </li>
+          <li class="breadcrumb-item active">Overview</li>
+        </ol>
+
+        <!-- Icon Cards-->
+        <div class="row">
+          <div class="col-xl-3 col-sm-6 mb-3">
+            <div class="card text-white bg-primary o-hidden h-100">
+              <div class="card-body">
+                <div class="card-body-icon">
+                  <i class="fas fa-fw fa-comments"></i>
+                </div>
+                <div class="stat-panel text-center">
+<?php 
+$sql ="SELECT driver_id from ibjt_driver";
+$query = $dbh -> prepare($sql);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+$bg=$query->rowCount();
+?>
+                          <div class="stat-panel-number h1 "><?php echo htmlentities($bg);?></div>
+                          <div class="stat-panel-title text-uppercase">Total Users</div>
+                        </div>
+                      </div>
+                      <a href="user_list.php" class="block-anchor panel-footer">Full Detail <i class="fa fa-arrow-right"></i></a>
+                    </div>
+          </div>
+
+          <!-- Daghan Pa ang E Edit-->
+          <div class="col-xl-3 col-sm-6 mb-3">
+            <div class="card text-white bg-warning o-hidden h-100">
+              <div class="card-body">
+                <div class="card-body-icon">
+                  <i class="fas fa-users"></i>
+                </div>
+               <div class="stat-panel text-center">
+<?php 
+$sql ="SELECT driver_id from ibjt_driver";
+$query = $dbh -> prepare($sql);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+$bg=$query->rowCount();
+?>
+                          <div class="stat-panel-number h1 "><?php echo htmlentities($bg);?></div>
+                          <div class="stat-panel-title text-uppercase">Total Drivers</div>
+                        </div>
+                      </div>
+                      <a href="user_list.php" class="block-anchor panel-footer">Full Detail <i class="fa fa-arrow-right"></i></a>
+                </span>
+              </a>
+            </div>
+          </div>
+          <div class="col-xl-3 col-sm-6 mb-3">
+            <div class="card text-white bg-success o-hidden h-100">
+              <div class="card-body">
+                <div class="card-body-icon">
+                  <i class="far fa-envelope"></i>
+                </div>
+                <div class="stat-panel text-center">
+<?php 
+$sql ="SELECT user_id from users";
+$query = $dbh -> prepare($sql);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+$bg=$query->rowCount();
+?>
+                          <div class="stat-panel-number h1 "><?php echo htmlentities($bg);?></div>
+                          <div class="stat-panel-title text-uppercase">Total Users</div>
+                        </div>
+                      </div>
+                       <a class="card-footer text-white clearfix small z-1" href="#">
+                <span class="float-left">View Details</span>
+                <span class="float-right">
+                  <i class="fas fa-angle-right"></i>
+                </span>
+              </a>
+            </div>
+          </div>
+          <div class="col-xl-3 col-sm-6 mb-3">
+            <div class="card text-white bg-danger o-hidden h-100">
+              <div class="card-body">
+                <div class="card-body-icon">
+                  <i class="fas fa-user-slash"></i>
+                </div>
+                <div class="mr-5">0 Deleted Users!</div>
+              </div>
+              <a class="card-footer text-white clearfix small z-1" href="#">
+                <span class="float-left">View Details</span>
+                <span class="float-right">
+                  <i class="fas fa-angle-right"></i>
+                </span>
+              </a>
+            </div>
+          </div>
+        </div>
+
+
+      <!-- Sticky Footer -->
+      <footer class="sticky-footer">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Copyright © EEDMO 2019</span>
+          </div>
+        </div>
+      </footer>
+
+    </div>
+    <!-- /.content-wrapper -->
+
+  </div>
+  <!-- /#wrapper -->
+
+  <!-- Scroll to Top Button-->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
+  <!-- Logout Modal-->
+  <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+        <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+        <div class="modal-footer">
+          <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+          <a class="btn btn-primary" href="login.html">Logout</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Page level plugin JavaScript-->
+  <script src="vendor/chart.js/Chart.min.js"></script>
+  <script src="vendor/datatables/jquery.dataTables.js"></script>
+  <script src="vendor/datatables/dataTables.bootstrap4.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="js/sb-admin.min.js"></script>
+
+  <!-- Demo scripts for this page-->
+  <script src="js/demo/datatables-demo.js"></script>
+  <script src="js/demo/chart-area-demo.js"></script>
+
+</body>
+</html>
+<?php } ?>
