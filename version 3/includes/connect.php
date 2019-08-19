974cc3f70b9c70b119d16af64a6aed534221fb3b
knightbreaker1 <48872382+knightbreaker1@users.noreply.github.com>
2019-08-19 12:14:08 +0800

    Add files via upload

--- version 3/includes/connect.php
+++ version 3/includes/connect.php
@@ -1,12 +1,20 @@
 <?php
-
- $host ="localhost";
- $username = "root";
- $password = "";
- $dbname = "eedmo_database";
-
-
- $con = mysqli_connect($host, $username, $password, $dbname);
-
-
- ?>
+	$username = 'root';
+	$password = '';
+	$host = 'localhost';
+	$database_name = 'eedmo_database';
+	
+	date_default_timezone_set("Asia/Manila");
+	
+	$conn = mysqli_connect($host, $username, $password, $database_name); 
+	if(!$conn){
+		echo 'Error: connecting to database'.mysql_error();
+	}
+	
+	$connect = mysqli_connect($host, $username, $password, $database_name); 
+	if(!$connect){
+		echo 'Error: connecting to database'.mysql_error();
+	}
+	
+	
+?>
---
+++ version 3/login.php
@@ -0,0 +1,106 @@
+<?php
+  session_start();
+  include ('includes/connect.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>EEDMO - Login</title>
+
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+  <link href="css/sb-admin.css" rel="stylesheet">
+
+</head>
+
+<body class="bg-dark">
+
+  <div class="container">
+    <div class="card card-login mx-auto mt-5">
+      <div class="card-header">Login</div>
+      <div class="card-body">
+        
+
+        <form method="post">
+
+          <div class="form-group">
+            <div class="form-label-group">
+              <input type="text" name="username" class="form-control" placeholder="Username" required="required" autofocus="autofocus">
+              <label for="username">Username</label>
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="form-label-group">
+              <input type="password" name="password" class="form-control" placeholder="Password" required="required">
+              <label for="password">Password</label>
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="checkbox">
+              <label>
+                <input type="checkbox" value="remember-me">
+                Remember Password
+              </label>
+            </div>
+          </div>
+          <button class="btn btn-primary btn-block"  input type="submit" name="login" type="login">LOGIN</button>
+        </form>
+        <div class="text-center">
+          <a class="d-block small mt-3" href="register.html">Register an Account</a>
+          <a class="d-block small" href="forgot-password.html">Forgot Password?</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+</body>
+</html>
+
+<?php
+
+if(isset($_POST['login'])) {        
+
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  $sql_admin = "SELECT * FROM admin WHERE username = '$username' and password='$password'";
+  $result_admin = $conn->query($sql_admin);
+
+  $sql_users = "SELECT * FROM users WHERE username = '$username' and password='$password'";
+  $result_users = $conn->query($sql_users);
+
+
+  if($result_admin->num_rows > 0) {
+    $row = $result_admin->fetch_assoc();     
+
+    session_start();  
+    $_SESSION['name'] = $row['name'];
+    $_SESSION['admin_id'] = $row['admin_id'];
+    echo "<script>window.open('admin/admin_index.php','_self');</script>";
+  }
+  else if($result_users->num_rows > 0) {
+    $row = $result_users->fetch_assoc();     
+
+    session_start();  
+    $_SESSION['name'] = $row['name'];
+    $_SESSION['user_id'] = $row['user_id'];
+    echo "<script>window.open('index.php','_self');</script>";
+  }else{
+    echo "<script>alert('Invalid Username or Password!');</script>";       
+  }
+  
+} 
+?>
